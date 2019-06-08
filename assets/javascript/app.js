4293ff57d9e10d91a12019d2ffdf7bc620190c1d
Andrew Osborn <andrewosborn101@gmail.com>
2019-06-07 19:49:09 -0500

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,15 @@
 
 var audio = new Audio("http://music.ogg" ) ;
+var clickCounter = 0;
 
 function fart(){
     document.getElementById("farter").style.color="yellow";
+    clickCounter= clickCounter + 1;
+    console.log(clickCounter);
+    document.getElementById("counter").innerHTML = clickCounter;
 
 };
+
+
+
+console.log(clickCounter);
